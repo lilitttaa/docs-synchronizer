71b70d8335276c3590c68270ba02448a15cdb4d3
zqz <qingzhe.zhou@yingxiong.com>
2024-05-14 21:26:30 +0800

    [feat] Convert a simple test md.

--- src/docs_synchronizer/__init__.py
+++ src/docs_synchronizer/__init__.py
@@ -1,2 +1,5 @@
-def add(a: int, b: int) -> int:
-    return a + b
+import markdown2
+
+class Md2HtmlConverter:
+    def convert(self, markdown_str: str) -> str:
+        return markdown2.markdown(markdown_str)
