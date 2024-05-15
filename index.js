5b9529d5da89f6535d40540563dc027b2149d894
zqz <qingzhe.zhou@yingxiong.com>
2024-05-15 13:56:35 +0800

    [refactor] cleanup the code.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-
-
-// CJS
 const { Notebook } = require('crossnote');
 const fs = require('fs').promises;
 const path = require('path');
-// ESM
-// import { Notebook } from "crossnote"
 
 async function exportHtmlFromMarkdown(dirPath, fileName) {
     const notebook = await Notebook.init({
@@ -19,92 +14,93 @@ async function exportHtmlFromMarkdown(dirPath, fileName) {
         },
     });
 
-    // Get the markdown engine for a specific note file in your notebook.
-    const engine = notebook.getNoteMarkdownEngine(fileName);
-
-    // html export
-    await engine.htmlExport({ offline: false, runAllCodeChunks: true });
+    await notebook.getNoteMarkdownEngine(fileName).htmlExport({ offline: false, runAllCodeChunks: true });
 }
 
 async function uploadMd2Blog(mdDir, mdFileName, blogRootDir) {
-    const currentDir = process.cwd();
-    const cacheDir = path.join(currentDir, "cache");
+    const currentDirPath = process.cwd();
+    const cacheDirPath = path.join(currentDirPath, "cache");
+    const htmlFilePath = path.join(cacheDirPath, mdFileName.replace(".md", ".html"));
+    const fileNameWithoutExt = mdFileName.replace(".md", "");
+    const blogOutputFilePath = path.join(blogRootDir, 'posts', mdFileName.replace(".md", ".html"));
+    const blogImageDirPath = path.join(blogRootDir, "public", "images", fileNameWithoutExt);
 
-    // Create the cache directory.
-    await fs.mkdir(cacheDir, { recursive: true });
+    await fs.mkdir(cacheDirPath, { recursive: true });
+    await copyMdToLocalCacheDir(mdDir, mdFileName, cacheDirPath);
+    await exportHtmlFromMarkdown(cacheDirPath, mdFileName);
+    await remapHtmlFileImgUrls(htmlFilePath, fileNameWithoutExt);
+    await fs.copyFile(htmlFilePath, blogOutputFilePath);
+    await createACleanDir(blogImageDirPath);
+    await copyImagesToBlog(mdDir, blogImageDirPath);
+    await fs.rmdir(cacheDirPath, { recursive: true });
+    
+}
 
-    // Copy md to local cache dir
-    const mdFilePath = path.join(mdDir, mdFileName);
-    const cacheMdFilePath = path.join(cacheDir, mdFileName);
-    await fs.copyFile(mdFilePath, cacheMdFilePath);
-
-    // export Html from markdown
-    await exportHtmlFromMarkdown(cacheDir, mdFileName);
-
-    // Add '/images/' prefix to all image url in the html.
-    const htmlFilePath = path.join(cacheDir, mdFileName.replace(".md", ".html"));
-    let html = await fs.readFile(htmlFilePath, "utf-8");
-
-    const mdFileNameWithoutExt = mdFileName.replace(".md", "");
-    html = html.replace(/<img src="(?<url>[^"]+)"/g, `<img src="/images/${mdFileNameWithoutExt}/$<url>"`);
-    await fs.writeFile(htmlFilePath, html);
-
-    // Move the exported html file to the blog directory.
-    const blogHtmlFilePath = path.join(blogRootDir, 'posts', mdFileName.replace(".md", ".html"));
-    await fs.copyFile(htmlFilePath, blogHtmlFilePath);
-
-    // Move all images to the blog directory.
-    const blogImageDir = path.join(blogRootDir, "public", "images", mdFileNameWithoutExt);
-    await fs.mkdir(blogImageDir, { recursive: true });
-
-    // Clear the blogImageDir
-    await fs.rmdir(blogImageDir, { recursive: true });
-    await fs.mkdir(blogImageDir, { recursive: true });
-
-    const files = await fs.readdir(mdDir);
-    for (const file of files) {
-        if (file.endsWith(".png") || file.endsWith(".jpg") || file.endsWith(".jpeg") || file.endsWith(".gif") || file.endsWith(".svg")) {
-            await fs.copyFile(path.join(mdDir, file), path.join(blogImageDir, file));
-        }
-    }
-
-    // Clear the cache directory.
-    await fs.rmdir(cacheDir, { recursive: true });
-
-    // Git commit and push the blog directory.
-    const git = require('simple-git')(blogRootDir);
+async function uploadBlog(blogRootDirPath) {
+    console.log("Uploading blog...")
+    const git = require('simple-git')(blogRootDirPath);
     await git.add(".");
     await git.commit("Update blog");
     await git.push("origin", "master");
 }
 
+async function copyImagesToBlog(mdDir, blogImageDirPath) {
+    const files = await fs.readdir(mdDir);
+    for (const file of files) {
+        if (file.endsWith(".png") || file.endsWith(".jpg") || file.endsWith(".jpeg") || file.endsWith(".gif") || file.endsWith(".svg")) {
+            await fs.copyFile(path.join(mdDir, file), path.join(blogImageDirPath, file));
+        }
+    }
+}
 
+async function createACleanDir(blogImageDirPath) {
+    await fs.mkdir(blogImageDirPath, { recursive: true });
+    await fs.rmdir(blogImageDirPath, { recursive: true });
+    await fs.mkdir(blogImageDirPath, { recursive: true });
+}
+
+async function remapHtmlFileImgUrls(htmlFilePath, fileNameWithoutExt) {
+    let html = await fs.readFile(htmlFilePath, "utf-8");
+    html = html.replace(/<img src="(?<url>[^"]+)"/g, `<img src="/images/${fileNameWithoutExt}/$<url>"`);
+    await fs.writeFile(htmlFilePath, html);
+}
+
+async function copyMdToLocalCacheDir(mdDir, mdFileName, cacheDir) {
+    const mdFilePath = path.join(mdDir, mdFileName);
+    const cacheMdFilePath = path.join(cacheDir, mdFileName);
+    await fs.copyFile(mdFilePath, cacheMdFilePath);
+}
+
+function getAllPubMdFiles(files) {
+    let pubMdFiles = []
+    for (const file of files) {
+        const fileName = path.basename(file);
+        if (fileName.startsWith("Pub_") && fileName.endsWith(".md")) {
+            pubMdFiles.push({
+                dir: path.dirname(file),
+                fileName: fileName,
+            });
+        }
+    }
+    return pubMdFiles;
+}
 
 async function main() {
     const config = require('./config.json');
     const mdRootDirPath = config.mdRootDirPath;
     const blogRootDirPath = config.blogRootDirPath;
 
-    const files = await fs.readdir(mdRootDirPath, { recursive: true });
-    const mdFiles = [];
-    for (const file of files) {
-        const fileName = path.basename(file);
-        console.log(file);
-        if (fileName.startsWith("Pub_") && fileName.endsWith(".md")) {
-            mdFiles.push({
-                dir: path.dirname(file),
-                fileName: fileName,
-            });
-        }
-    }
+    const pubMdFiles = getAllPubMdFiles(await fs.readdir(mdRootDirPath, { recursive: true }));
 
-
-    for (const mdFile of mdFiles) {
+    for (const mdFile of pubMdFiles) {
+        console.log("Processing: ", mdFile.fileName);
         const dirPath = path.join(mdRootDirPath, mdFile.dir);
         await uploadMd2Blog(dirPath, mdFile.fileName, blogRootDirPath);
     }
 
-    return process.exit();
+    await uploadBlog(blogRootDirPath);
 }
 
 main();
+
+
